b4eb2a3c79ef75db8000eb44dca45722683d7f53
LaLa-Lisa <lyohich999@ya.ru>
2020-03-28 19:59:16 +0300

    ееееее, изменения

--- Nuar_Start/Nuar_Start/Source.cpp
+++ Nuar_Start/Nuar_Start/Source.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <vector>
+
+class NCharacter
+{
+public:
+
+
+};
+
+
+class NField {
+public:
+	void move(int column, int row);
+private:
+	std::vector<std::vector<NCharacter>> field;
+};
+
+class NDetective
+{
+	virtual void makeSurvey();
+};
+__interface NKiller
+{
+	void killSomeone();
+};
+class NPlayer sealed : NDetective, NKiller{
+public:
+	void NDetective::makeSurvey();
+	void killSomeone();
+private:
+	bool isDead = false;
+};
 
 int main() {
-	std::cout << "redone\n";
-	std::cout << "Is it works?\n";
+	NDetective A = new NPlayer;
 	return 0;
 }
